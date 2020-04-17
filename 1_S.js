e1c13cbf6df8293456219a3cfbb1f97ed6753395
Offych <off@tut.by>
2020-04-17 10:23:13 +0300

    SOLID Principles. S

---
+++ 1_S.js
@@ -0,0 +1,77 @@
+//Single Responsibility Principle
+
+class News {
+  constructor(title, text) {
+    this.title = title;
+    this.text = text;
+    this.modified = false;
+  }
+
+  update(text) {
+    this.text = text;
+    this.modified = true;
+  }
+
+  /*   toHTML() {
+    return `
+        <div class='news'>
+            <h1>${this.title}</h1>
+            <p>${this.text}</p>
+        </div>
+      `;
+  }
+
+  toJSON() {
+    return JSON.stringify(
+      {
+        title: this.title,
+        text: this.text,
+        modified: this.modified,
+      },
+      null,
+      2
+    ); // форматированный JSON
+  } */
+}
+
+class NewsPrinter {
+  constructor(news) {
+    this.news = news;
+  }
+
+  html() {
+    return `
+        <div class='news'>
+            <h1>${this.news.title}</h1>
+            <p>${this.news.text}</p>
+        </div>`;
+  }
+
+  json() {
+    return JSON.stringify(
+      {
+        title: this.news.title,
+        text: this.news.text,
+        modified: this.news.modified,
+      },
+      null,
+      2
+    );
+  }
+
+  xml() {
+    return `
+        <news>
+        <title>${this.news.title}</title>
+        <text>${this.news.text}</text>
+        </news>
+      `;
+  }
+}
+
+const printer = new NewsPrinter(new News("Good news", "Will continue"));
+
+//const news = new News("Good news", "Will continue");
+console.log(printer.html());
+console.log(printer.json());
+console.log(printer.xml());
